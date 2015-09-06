12bcc36ecce6217037b659e4be1ca584b5a46455
Jianhui Zhu <jianhuizhu1987@gmail.com>
2015-09-05 21:42:19 -0400

    Refactoring and remove lib not necessary, more comments to be add

--- app/src/main/java/com/tofohack/jianhui_zhu/tablayoutwithviewpagerexample/PageFragment.java
+++ app/src/main/java/com/tofohack/jianhui_zhu/tablayoutwithviewpagerexample/PageFragment.java
@@ -10,8 +10,6 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.squareup.picasso.Picasso;
-
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
@@ -19,25 +17,26 @@ import butterknife.ButterKnife;
  * Created by jianhuizhu on 15-09-05.
  */
 public class PageFragment extends Fragment {
-     Context context;
+    Context context;
     @Bind(R.id.textInPage)
     TextView textView;
     @Bind(R.id.imageInPage)
     ImageView imageView;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.page_fragment,container,false);
-        ButterKnife.bind(this,view);
+        View view = inflater.inflate(R.layout.page_fragment, container, false);
+        ButterKnife.bind(this, view);
         return view;
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        int pageNum=getArguments().getInt("Page");
-        textView.setText("Squirrel "+(pageNum+1));
-        switch(pageNum){
+        int pageNum = getArguments().getInt("Page");
+        textView.setText("Squirrel " + (pageNum + 1));
+        switch (pageNum) {
             case 0:
                 imageView.setImageResource(R.drawable.squirrel1);
                 break;
@@ -49,10 +48,11 @@ public class PageFragment extends Fragment {
                 break;
         }
     }
-    public static PageFragment newInstance(Context context,int pageNum){
-        PageFragment pageFragment=(PageFragment)instantiate(context,PageFragment.class.getName());
-        pageFragment.context=context;
-        Bundle bundle=new Bundle();
+
+    public static PageFragment newInstance(Context context, int pageNum) {
+        PageFragment pageFragment = (PageFragment) instantiate(context, PageFragment.class.getName());
+        pageFragment.context = context;
+        Bundle bundle = new Bundle();
         bundle.putInt("Page", pageNum);
         pageFragment.setArguments(bundle);
         return pageFragment;
--- app/src/main/java/com/tofohack/jianhui_zhu/tablayoutwithviewpagerexample/PageFragmentAdapter.java
+++ app/src/main/java/com/tofohack/jianhui_zhu/tablayoutwithviewpagerexample/PageFragmentAdapter.java
@@ -9,16 +9,17 @@ import android.support.v4.app.FragmentPagerAdapter;
  * Created by jianhuizhu on 15-09-05.
  */
 public class PageFragmentAdapter extends FragmentPagerAdapter {
-    private final int numOfPage=3;
+    private final int numOfPage = 3;
     private Context context;
-    public PageFragmentAdapter(FragmentManager fm,Context context) {
+
+    public PageFragmentAdapter(FragmentManager fm, Context context) {
         super(fm);
-        this.context=context;
+        this.context = context;
     }
 
     @Override
     public Fragment getItem(int position) {
-        return PageFragment.newInstance(context,position);
+        return PageFragment.newInstance(context, position);
     }
 
     @Override
